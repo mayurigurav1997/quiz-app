90628d96310de3b8c8632c07443877bf58c59b63
Mayuri <mayurigurav1997@gmail.com>
2024-03-22 12:08:26 +0530

    shuffled answers by creating deep copy and sort method also added quiz completed

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,27 @@
 import React, { useState } from 'react'
 import QUESTIONS from "../questions"
+import quizCompleteImg from "../assets/quiz-complete.png"
 
 const Quiz = () => {
+
     const [userAnswers, setUserAnswers] = useState([])
     //derive the state
     const activeQuestionIndex = userAnswers.length;
+
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length
+    if (quizIsComplete) {
+        return <div id="summary">
+            <img src={quizCompleteImg} alt="Trophy Icon" />
+            <h2>Quiz Completed!</h2>
+        </div>
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+    shuffledAnswers.sort(() => {
+        Math.random() - 0.5
+    })
+
+
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswers) => {
@@ -20,7 +37,7 @@ const Quiz = () => {
                 </h2>
                 <ul id="answers">
                     {
-                        QUESTIONS[activeQuestionIndex].answers.map((answer) => {
+                        shuffledAnswers.map((answer) => {
                             return <li key={answer} className='answer'>
                                 <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                             </li>
