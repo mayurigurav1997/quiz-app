b267a70f8f3cfc111e122cd4ba8948b5d61755c9
Mayuri <mayurigurav1997@gmail.com>
2024-03-22 18:16:17 +0530

    Question component created

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,64 +1,47 @@
 import React, { useCallback, useState } from "react";
 import QUESTIONS from "../questions";
 import quizCompleteImg from "../assets/quiz-complete.png";
-import QuestionTimer from "./QuestionTimer";
-import Answers from "./Answers";
+
 import Question from "./Question";
 
 const Quiz = () => {
-    const [answerState, setAnswerState] = useState("");
-    const [userAnswers, setUserAnswers] = useState([]);
-    //derive the state
-    const activeQuestionIndex =
-        answerState === "" ? userAnswers.length : userAnswers.length - 1;
+  const [userAnswers, setUserAnswers] = useState([]);
+  //derive the state
+  const activeQuestionIndex = userAnswers.length;
 
-    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
-    if (quizIsComplete) {
-        return (
-            <div id="summary">
-                <img src={quizCompleteImg} alt="Trophy Icon" />
-                <h2>Quiz Completed!</h2>
-            </div>
-        );
-    }
-
-    const handleSelectAnswer = useCallback(
-        function handleSelectAnswer(selectedAnswer) {
-            setAnswerState("answered");
-            setUserAnswers((prevUserAnswers) => {
-                return [...prevUserAnswers, selectedAnswer];
-            });
-            setTimeout(() => {
-                if (selectedAnswer == QUESTIONS[activeQuestionIndex].answers[0]) {
-                    setAnswerState("correct");
-                } else {
-                    setAnswerState("wrong");
-                }
-                setTimeout(() => {
-                    setAnswerState("");
-                }, 2000);
-            }, 1000);
-        },
-        [activeQuestionIndex]
-    );
-
-    const handleSkipAnswer = useCallback(
-        () => handleSelectAnswer(null),
-        [handleSelectAnswer]
-    );
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+  if (quizIsComplete) {
     return (
-        <div id="quiz">
-            <Question
-                key={activeQuestionIndex}
-                questionText={QUESTIONS[activeQuestionIndex].text}
-                answers={QUESTIONS[activeQuestionIndex].answers}
-                onSelectAnswer={handleSelectAnswer}
-                answerState={answerState}
-                selectedAnswer={userAnswers[userAnswers.length - 1]}
-                onSkipAnswer={handleSkipAnswer}
-            />
-        </div>
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy Icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
     );
+  }
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
+    </div>
+  );
 };
 
 export default Quiz;
