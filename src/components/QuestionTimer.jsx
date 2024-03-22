f0fa8c59ea31063e5c4ad0a37de30fd4d5ee13a5
Mayuri <mayurigurav1997@gmail.com>
2024-03-22 19:28:54 +0530

    Summary component created

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,35 +1,40 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-const QuestionTimer = ({ timeout, onTimeout }) => {
-    const [remainingTime, setRemainingTime] = useState(timeout)
+const QuestionTimer = ({ timeout, onTimeout, mode }) => {
+  const [remainingTime, setRemainingTime] = useState(timeout);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            console.log("timeout set")
-            onTimeout()
-        }, timeout)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("timeout set");
+      onTimeout();
+    }, timeout);
 
-        return () => {
-            console.log("removed timeout")
-            clearTimeout(timer)
-        }
-    }, [timeout, onTimeout])
+    return () => {
+      console.log("removed timeout");
+      clearTimeout(timer);
+    };
+  }, [timeout, onTimeout]);
 
-    useEffect(() => {
-        const timerId = setInterval(() => {
-            console.log("timeinterval set")
-            setRemainingTime(prevRemainingTime => prevRemainingTime - 100)
-        }, 100)
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      console.log("timeinterval set");
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
 
-        return () => {
-            console.log("removed timeinterval")
-            clearInterval(timerId)
-        }
-    }, [])
+    return () => {
+      console.log("removed timeinterval");
+      clearInterval(timerId);
+    };
+  }, []);
 
-    return (
-        <progress id="qusetion-time" value={remainingTime} max={timeout} />
-    )
-}
+  return (
+    <progress
+      id="qusetion-time"
+      value={remainingTime}
+      max={timeout}
+      className="mode"
+    />
+  );
+};
 
-export default QuestionTimer
+export default QuestionTimer;
